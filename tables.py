0b6df2701aa8cc46d50f96051b1b0a0f4fb6e6bc
Daniel Daza <dfdazac@gmail.com>
2025-06-10 15:47:32 +0200

    Update tables

--- tables.py
+++ tables.py
@@ -1,5 +1,7 @@
 from collections import OrderedDict
 
+from quack.qto.query import name_answer_dict
+
 import pandas as pd
 
 def process_files(file_dict, metric_name):
@@ -8,6 +10,9 @@ def process_files(file_dict, metric_name):
     for method, file in file_dict.items():
         metrics = {}
         metrics["method"] = method
+        for m in name_answer_dict:
+            metrics[m] = None
+
         all_values = []
         with open(file, 'r') as f:
             for line in f:
@@ -36,3 +41,13 @@ fb15k237_results = OrderedDict([
 
 print(process_files(fb15k237_results, "cumulative_pairwise_accuracy").to_latex(index=False))
 print(process_files(fb15k237_results, "cumulative_mrr_hard").to_latex(index=False))
+
+hetionet_results = OrderedDict([
+    ("QTO", "results/hetionet_10_0.001_default_test_mixed_1746908572_wlxyi0d7/all_metrics_test_mixed.txt"),
+    ("Cosine", "results/hetionet_10_0.001_cosine_0.1_0.9_test_mixed_1746909609_14rps2d8/all_metrics_test_mixed.txt"),
+    ("Ranknet", "results/hetionet_10_0.001_ranknet_test_mixed_1747222015_y13wghbh/all_metrics_test_mixed.txt"),
+    ("NQR", "results/hetionet_10_0.001_nqr_0.001_test_mixed_1747027748_dj60prrp/all_metrics_test_mixed.txt")
+    ])
+
+print(process_files(hetionet_results, "cumulative_pairwise_accuracy").to_latex(index=False))
+print(process_files(hetionet_results, "cumulative_mrr_hard").to_latex(index=False))
